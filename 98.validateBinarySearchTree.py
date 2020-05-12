3ed932fcfd9bcc4421baf93b6e3eab3a797c8412
SvizzerChow <mysvizzera@outlook.com>
2020-05-12 21:20:10 +0800

    @update 98

--- 98.validateBinarySearchTree.py
+++ 98.validateBinarySearchTree.py
@@ -4,29 +4,27 @@ class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         if root is None:
             return True
-        return self.dfsMax(root) is not None
+        return self.dfs(root, None) is not None
 
-    def dfsMax(self, root):
-        maxValue = root.val
-        minValue = root.val
+    def dfs(self, root, before):
+        last = None
         if root.left is not None:
-            value = self.dfsMax(root.left)
-            if value is None or (value[0] >= root.val or value[1] >= root.val):
+            temp = self.dfs(root.left, before)
+            if temp is None or temp >= root.val:
                 return None
-            minValue = value[0]
+        if before is None or root.val > before:
+            last = root.val
+        else:
+            return None
         if root.right is not None:
-            value = self.dfsMax(root.right)
-            if value is None or (value[0] <= root.val or value[1] <= root.val):
+            temp = self.dfs(root.right, last)
+            if temp is None or temp <= root.val:
                 return None
-            maxValue = value[1]
-        return [minValue, maxValue]
-
-    def check(self, value, val):
-        if value[0] >=val:
-            return True
-        if value[1] <= val:
-            return True
-        return False
+            last = temp
+        return last
 
 
-print(Solution().isValidBST(TreeNode.stringToTreeNode("[10,5,15,null,null,6,20]")))
+
+
+
+print(Solution().isValidBST(TreeNode.stringToTreeNode("[5,1,4,null,null,3,6]")))
