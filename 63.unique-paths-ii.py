fba3f89df9d38e469e792a9bd4e4b3a30bce6a5c
SvizzerChow <mysvizzera@outlook.com>
2020-07-06 16:41:38 +0800

    @update 63

--- 63.unique-paths-ii.py
+++ 63.unique-paths-ii.py
@@ -5,22 +5,21 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         if len(obstacleGrid) < 1:
             return 0
-        counter = [[0] * len(obstacleGrid[0])] * len(obstacleGrid)
-        counter[0][0] = 1
         for i in range(len(obstacleGrid)):
             for j in range(len(obstacleGrid[0])):
                 if obstacleGrid[i][j] == 1:
-                    counter[i][j] = 0
+                    obstacleGrid[i][j] = 0
                     continue
                 if i == j == 0:
+                    obstacleGrid[i][j] = 1
                     continue
                 total = 0
                 if i - 1 >= 0:
-                    total += counter[i-1][j]
+                    total += obstacleGrid[i-1][j]
                 if j - 1 >= 0:
-                    total += counter[i][j-1]
-                counter[i][j] = total
-        return counter[-1][-1]
+                    total += obstacleGrid[i][j-1]
+                obstacleGrid[i][j] = total
+        return obstacleGrid[-1][-1]
 
 
 obstacleGrid = [
