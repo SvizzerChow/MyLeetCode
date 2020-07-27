25c224a55a1985e4bb125aeb8447762111ebba0c
SvizzerChow <mysvizzera@outlook.com>
2020-07-27 16:21:52 +0800

    @UPDATE 1025

--- 1025.divisor-game.py
+++ 1025.divisor-game.py
@@ -1,16 +1,6 @@
 class Solution:
     def divisorGame(self, N: int) -> bool:
-        flags = [[False, False] for _ in range(N+1)]
-        if N == 1:
-            return False
-        for i in range(2, N+1):
-            for j in range(i - 1, 0, -1):
-                if i % j == 0:
-                    temp = i - j
-                    flags[i][0] = flags[i][0] or (flags[temp][1])
-                    flags[i][1] = flags[i][1] or (flags[temp][0] or not flags[temp][1])
-        print(flags)
-        return flags[N][1]
+        return N % 2 == 0
 
 
-print(Solution().divisorGame(4))
+print(Solution().divisorGame(5))
