b9c9d50ca2c14dd3a017f2d1124abf5dad818317
SvizzerChow <mysvizzera@outlook.com>
2020-08-19 17:16:27 +0800

    @update 395

--- 395.longest-substring-with-at-least-k-repeating-characters.py
+++ 395.longest-substring-with-at-least-k-repeating-characters.py
@@ -1,58 +1,16 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
+        if len(s) < k:
+            return 0
         counterMap = {}
         for i in range(len(s)):
             if s[i] not in counterMap:
-                counterMap[s[i]] = []
-            counterMap[s[i]].append(i)
-        noChar = set()
+                counterMap[s[i]] = 0
+            counterMap[s[i]] += 1
         for cc in counterMap.keys():
-            if len(counterMap.get(cc)) < k:
-                noChar.add(cc)
-        if len(noChar) <= 0:
-            return len(s)
-        if len(noChar) == len(s):
-            return 0
-        start = index = 0
-        end = 1
-        length = len(s)
-        maxLength = 0
-        having = set()
-        while index < length:
-            if s[index] in noChar:
-                start = index = start + 1
-                end = start + 1
-                having = set()
-                continue
-            if s[index] not in having:
-                temp = self._getIndex(index, counterMap[s[index]], k)
-                if temp == -1:
-                    start = index = start + 1
-                    end = start + 1
-                    having = set()
-                    continue
-                if end < temp:
-                    end = temp
-                having.add(s[index])
-            if index == end:
-                l = end - start + 1
-                if l > maxLength and l >= k:
-                    print(start, end, s[start:end + 1])
-                    maxLength = l
-                end += 1
-            index += 1
-        return maxLength
-
-    def _getIndex(self, i, temp, k):
-        p = 0
-        for jj in range(len(temp)):
-            if temp[jj] == i:
-                p = jj
-                break
-        if p + k - 1 < len(temp):
-            return temp[p + k - 1]
-        return -1
-
+            if counterMap.get(cc) < k:
+                return max(self.longestSubstring(temp, k) for temp in s.split(cc))
+        return len(s)
 
 
 print(Solution().longestSubstring("baaabcb", 3))
