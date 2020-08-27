9ba2edb0be709073ea21c611751bf0e736b7719c
SvizzerChow <mysvizzera@outlook.com>
2020-08-27 10:34:04 +0800

    @update 17

--- 17.letter-combinations-of-a-phone-number.py
+++ 17.letter-combinations-of-a-phone-number.py
@@ -3,19 +3,35 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        return self._getStrs(digits)
+        if digits is None or digits == '':
+            return []
+        numMap = {}
+        for i in range(2, 10):
+            numMap[str(i)] = self._getStrs(i)
+        print(numMap)
+        return self._merge(digits, 0, numMap)
 
+    def _merge(self, s, index, numMap):
+        now = numMap[s[index]]
+        if index == len(s)-1:
+            return now
+        after = self._merge(s, index+1, numMap)
+        result = []
+        for i in range(len(now)):
+            temp = now[i]
+            for ss in after:
+                result.append(temp + ss)
+        return result
 
-
-    def _getStrs(self, s):
+    def _getStrs(self, n):
         k = 3
-        if s == '9':
-            k=4
-        n = ord(s) - 49
+        if n == 7 or n == 9:
+            k = 4
+        temp = 0 if n < 8 else 1
         result = []
         for i in range(k):
-            result.append(chr(97+(n-1)*3+i))
+            result.append(chr(97+(n-2)*3+i+temp))
         return result
 
 
-print(Solution().letterCombinations("2"))
+print(Solution().letterCombinations("23"))
