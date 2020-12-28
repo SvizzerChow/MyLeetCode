1458571c42b0e56cb7d7f83779e1ebc13a17bcb8
SvizzerChow <mysvizzera@outlook.com>
2020-12-28 16:38:10 +0800

    @update 121 股票1改为dp实现

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -5,24 +5,18 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) < 2:
             return 0
-        # [0, 0, 0]不交易，买入，卖出
-        buys = [[0, 0, 0] for _ in range(len(prices))]
-        buys[0][1] = -prices[0]
+        # [0, 0, 0]之前赚的的最大值，之前买入的最小，买入，卖出
+        buys = [[0, 0, 0, 0] for _ in range(len(prices))]
+        buys[0][1] = prices[0]
+        buys[0][2] = prices[0]
         for i in range(1, len(prices)):
-            buys[i][0] = max(buys[i-1][0], buys[i-1][1], buys[i-1][2])
-            buys[i][1] = -prices[i]
-            t = buys[i-1][0]
-            if t < 0:
-                t = max(t, buys[i-1][1])
-            else:
-                t = buys[i-1][1]
-            if t >= 0:
-                continue
-            buys[i][2] = prices[i] + t
-        return max(buys[-1][0], buys[-1][2])
+            buys[i][0] = max(buys[i-1][0], buys[i-1][-1])
+            buys[i][1] = min(buys[i-1][1], buys[i-1][2])
+            buys[i][2] = prices[i]
+            buys[i][3] = prices[i] - buys[i][1]
+        print(buys)
+        return max(buys[-1][0], buys[-1][3])
 
-
-
-print(Solution().maxProfit([2,7,1,4]))
+print(Solution().maxProfit([7,1,5,3,6,4]))
 
 
