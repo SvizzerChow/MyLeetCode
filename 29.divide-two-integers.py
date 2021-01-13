7157a2ef1edb542ec1aa43511929eb3b8777d237
SvizzerChow <mysvizzera@outlook.com>
2021-01-13 16:23:09 +0800

    @update 29 除法新实现

--- 29.divide-two-integers.py
+++ 29.divide-two-integers.py
@@ -5,14 +5,21 @@ class Solution:
         a = abs(dividend)
         b = abs(divisor)
         same = not (a == dividend) ^ (b == divisor)
-        result = 0
-        for i in range(32, -1, -1):
-            if a - (b << i) >= 0:
-                result += 2**i
-                a -= (b << i)
+        result = self.getNum(a, b)
+
         if result > (2**31 - 1) or result < -2**31:
             result = 2**31 - 1
         return result if same else -result
 
+    def getNum(self, dividend, divisor):
+        if dividend < divisor:
+            return 0
+        counter = 1
+        p = divisor
+        while p + p < dividend:
+            p = p + p
+            counter = counter + counter
+        return counter + self.getNum(dividend - p, divisor)
 
-print(Solution().divide(-100, -100))
+
+print(Solution().divide(1, -1))
