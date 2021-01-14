522ab850cb05b7bcf46d518f1785670d5d319cb2
SvizzerChow <mysvizzera@outlook.com>
2021-01-14 17:41:27 +0800

    @add 19 删除链表的倒数第几个

--- 19.remove-nth-node-from-end-of-list.py
+++ 19.remove-nth-node-from-end-of-list.py
@@ -3,19 +3,24 @@ from tools.ListNode import ListNode, stringToListNode, listNodeToString
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        l = []
-        p = head
-        while p is not None:
-            l.append(p)
-            p = p.next
-        end = l[-n+1] if n > 1 else None
-        before = l[-n-1] if len(l) - n - 1 >=0 else None
-        if before:
-            before.next = end
+        if head is None:
+            return None
+        fast = head
+        for i in range(n):
+            if fast is None:
+                return None
+            fast = fast.next
+        slow = head
+        pre = None
+        while fast is not None:
+            fast = fast.next
+            pre = slow
+            slow = slow.next
+        if pre is None:
+            head = slow.next
         else:
-            head = end
+            pre.next = slow.next
         return head
-
 
 node = stringToListNode("[1,2,3,4,5]")
 n = 2
