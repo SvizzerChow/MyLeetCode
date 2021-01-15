e2f774e8d41168b0fb6febd39346ddd24ddd8151
SvizzerChow <mysvizzera@outlook.com>
2021-01-15 11:18:11 +0800

    @update 567 双指针

--- 567.permutation-in-string.py
+++ 567.permutation-in-string.py
@@ -1,54 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s1) < len(s2):
             return False
         counter = {}
-        keys = set()
-        for s in s1:
-            if counter.get(s) is None:
-                counter[s] = [0, 0]
-            counter[s][0] += 1
-            keys.add(s)
-        stack = []
-        addIndex = 0
-        stackStart = 0
-        while addIndex < len(s2):
-            for i in range(addIndex, len(s2)):
-                ss = s2[i]
-                addIndex += 1
-                if ss in counter:
-                    counter[ss][1] += 1
-                    stack.append(i)
-                    if counter[ss][0] <= counter[ss][1] and ss in keys:
-                        keys.remove(ss)
-                    if len(keys) == 0:
+        tSet = set()
+        for c in s2:
+            tSet.add(c)
+            if counter.get(c) is None:
+                counter[c] = [0, 0]
+            counter[c][0] += 1
+        left = right = 0
+        while right < len(s1):
+            ch = s1[right]
+            if ch in counter:
+                counter[ch][1] += 1
+                if counter[ch][0] == counter[ch][1]:
+                    tSet.remove(ch)
+            right += 1
+            if len(tSet) == 0:
+                while True:
+                    temp = s1[left]
+                    if temp in counter and counter[temp][0] == counter[temp][1]:
                         break
-            if len(keys) > 0:
-                return False
-            if len(stack) < 2:
-                return True
-            for iii in range(stackStart, len(stack)):
-                if counter[s2[stack[iii]]][1] > counter[s2[stack[iii]]][0]:
-                    stackStart += 1
-                    counter[s2[stack[iii]]][1] -= 1
-                else:
-                    break
-            if len(s1) < len(stack) - stackStart:
-                continue
-            update = False
-            for ii in range(stackStart + 1, len(stack)):
-                if stack[ii] - stack[ii - 1] > 1:
-                    update = True
-                    for j in range(stackStart, ii):
-                        counter[s2[stack[j]]][1] = counter[s2[stack[j]]][1] - 1 if counter[s2[stack[j]]][1] > 1 else 0
-                        keys.add(s2[stack[j]])
-                        stackStart += 1
-            if not update and len(s1) == len(stack) - stackStart:
-                return True
-        return len(s1) == len(stack) - stackStart
+                    left += 1
+                    if temp in counter:
+                        counter[temp][1] -= 1
+                if (right - left) == len(s2):
+                    return True
+        return False
 
-s1 = "qiyjjmsiurfwzolbk"
-s2 = "sjfqzacmkijurdtkjsvcgvlapehhtxqypyrksepemqygkkwqruqgxhmssncuqnjhukwisexdfwgtbnxnvtzyjrvxvnexoqylquwvselycwlndpnstkusaqgtcqofznabnwxumvhmbbzpubpjbiuorxsqgccmwuxtzzmuiflydvwoshwbzmbzbvawqxnebksmhcezlemrkjasbgltnhwswmodlyjhpyrjfydvnugucmvemejxazfnjuyahbsyisquyeddwcdqqhfmldebhlcvqggzvysqbevtfvgztxoipycmoahotmfjzvycnxylelgvapprablydqhjzpbtzigeepznggnzvczvfmetuyernrjsijrbkqifwuzmoljylrogryeivdqrrqtmhgrvcyjkfbifxtzaacdpxnpbqlhjomktqgtzvoqixuntbnsyndkywkftqjbihdaodgdsxbyrabpjgrppegnomkkhfpyhtlhawbzvjjelayyxgimnsghqbybbsvofcgxhdumrpqdzmykeuzlyivzudshlomjhlkqnjmwwyyaxzsrtujzdokcrhaawlnwpxklgnzfgckujvntwzvupboahvbwjduaoojlvyldiokahefyppamdgrlwsaynuozppgqjaqmzpgrognbrvlupzsjkqhjopjilvjncigrczxwlojtgdqijcmumnhxvnvpbxoumtepzcjffpyiiryuwqbsrlprsnpvfbdlpcjalcoabmrsiemfstncgyspkmxskufzfheitdzzijhwqurywvypwzrbtygowqrhedtwgxcrvoubxqwwhqkyedzludawxknospjothpkcuzwntejjdjgwnajciplncmczthmjmhvbmanepldkechpkwmhcjytqiucweekeahnsjtllgwehclleiykibiihjyrpivpgxciyqitxwqmtncqbgtpogzlxyppdcbrwqtnhucsurkrkwcgapvcsavlfezhhdodgituntplwoqpuxawbftpqgybqwglkqbrslkcladxgilznmmurikhmivjhrpiuovbtucpufxltwnxtqkqsjvdpnkcjccjstpmssdsytfzlwgzcttfpjustxachhwzacpxexlbkpuqyqcvbhrqfcqynhgbkooaubyjatfltoryxesddapmzdqhizblmbivkbdamrbxrqyiyonhzhefvfhlqmtybxlkarfibkqcsdwovvpnlvdqubrewsdusrfromqlgmmqdchnbltxfskowkdzpndkvljvpfkazpbnwdosjmobdetqhfjutphqdsmrlazlbghkehntimokxjusticdnshqrlqxtwdooshkqvkdolkldknchzzhxkjisnnokjdrfhxcevjjuyyeczcvjdeoysxcqueasienqklyuaomeoqjkkmftmhieeuvktolbvazftzqqznonvejiidhbpkpejmqyvkytlncbpxjopomgomgxdktfxbkdbovtzkmvxbidzfjzpwxjvqrqscsppravwpjyevwjqwnwuoiamguqikctjqccnjqfcemyjjrcsxbdzxdgyokvbirlrtqublgoolziqttuunwtwmisdhhnzvazvpjrsfkcyjbaflfazmhttmaynpfqhragkrgzeeiqzwexozhj"
+
+s1 = "aaa"
+s2 = "a"
 
 
 print(Solution().checkInclusion(s1, s2))
