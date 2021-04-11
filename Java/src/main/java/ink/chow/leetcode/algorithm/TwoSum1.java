ea2e0fda6aac2151ff1fdad10e034a9d9f42304b
ChowSvizzer <mysvizzera@outlook.com>
2021-04-11 23:35:39 +0800

    第一题

---
+++ Java/src/main/java/ink/chow/leetcode/algorithm/TwoSum1.java
@@ -0,0 +1,29 @@
+package ink.chow.leetcode.algorithm;
+
+import java.util.*;
+
+public class TwoSum1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[0];
+        if (nums == null || nums.length == 0){
+            return result;
+        }
+        if (nums.length == 1 && target != nums[0]){
+            return result;
+        }
+        Map<Integer, Integer> cache = new HashMap<>();
+        for (int i =0; i < nums.length; i++){
+            if (cache.containsKey(target - nums[i])){
+                return new int[]{cache.get(target - nums[i]), i};
+            }
+            cache.put(nums[i], i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] data = new int[]{3, 3};
+        System.out.println(Arrays.toString(new TwoSum1().twoSum(data, 7)));
+    }
+}
